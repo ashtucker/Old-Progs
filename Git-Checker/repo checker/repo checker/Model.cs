fd7c2b4148eb92858fe9354b0a1617ed9f43ac15
Daria <jchnw@yandex.ru>
2019-04-28 18:27:12 +0300

    git-checker

---
+++ Git-Checker/repo checker/repo checker/Model.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repo_checker
+{
+    class Model
+    {
+        private List<string> _repAddresses;
+
+        public Model()
+        {
+            this._repAddresses = new List<string>();
+        }
+
+        public List<string> RepAddresses
+        {
+            get { return this._repAddresses; }
+        }
+
+        public bool CheckAddress(string address)
+        {
+            return this.RepAddresses.Contains(address);
+        }
+
+        public void AddAddress(string address)
+        {
+            this.RepAddresses.Add(address);
+        }
+    }
+}
---
+++ Git-Checker/repo checker/repo checker/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace repo_checker
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Form1 view = new Form1();
+            Controller controller = new Controller(view);
+            Application.Run(view);
+        }
+    }
+}
